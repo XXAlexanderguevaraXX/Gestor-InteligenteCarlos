44f8405458568ce164d012d7e4e5f405c8f4482b
Dust_dev <selensax12@gmail.com>
2025-12-10 02:47:38 -0600

    Añadida clase Curso.java

---
+++ src/Curso.java
@@ -0,0 +1,49 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Curso {
+    private String idCurso;
+    private String nombre;
+    private String docente;
+    private int cupoMaximo;
+    private int cuposDisponibles;
+    private int creditos;
+    private Set<String> areas; // Conjuntos
+
+    public Curso(String idCurso, String nombre, String docente, int cupoMaximo, int creditos, Set<String> areas) {
+        this.idCurso = idCurso;
+        this.nombre = nombre;
+        this.docente = docente;
+        this.cupoMaximo = cupoMaximo;
+        this.cuposDisponibles = cupoMaximo;
+        this.creditos = creditos;
+        this.areas = areas;
+    }
+
+    // Getters y Setters
+    public String getIdCurso() { return idCurso; }
+    public String getNombre() { return nombre; }
+    public int getCupoMaximo() { return cupoMaximo; }
+    public int getCuposDisponibles() { return cuposDisponibles; }
+    public Set<String> getAreas() { return areas; }
+
+    public boolean disminuirCupo() {
+        if (cuposDisponibles > 0) {
+            cuposDisponibles--;
+            return true;
+        }
+        return false;
+    }
+
+    public void aumentarCupo() {
+        if (cuposDisponibles < cupoMaximo) {
+            cuposDisponibles++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + idCurso + " | Nombre: " + nombre + " | Docente: " + docente +
+                " | Cupo: " + cuposDisponibles + "/" + cupoMaximo + " | Áreas: " + areas;
+    }
+}
